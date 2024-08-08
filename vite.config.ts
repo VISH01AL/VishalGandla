9ad28828a40233a5582e39d6e79028ef7adbc603
VISH01AL <g.vish1al@gmail.com>
2024-08-08 18:47:39 -0400

    fixed config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base:"./",
-// })
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
-});
+  base:"./",
+})
+
