9196fe530206bf7bb74c8c38e0f5ae91b3d58f0e
VISH01AL <g.vish1al@gmail.com>
2025-02-01 22:04:25 -0500

    added navbar and footer colors

--- src/Components/Core/Footer.tsx
+++ src/Components/Core/Footer.tsx
@@ -7,17 +7,17 @@ const Footer = () => {
   const {theme}=useTheme()
   return (
     <div className={`${Styles.container} ${Styles[`container_${theme}`]}`} >
-      <a target="_blank" href='https://www.linkedin.com/in/vish1al/' className={Styles.mailtip}>   
+      <a target="_blank" href='https://www.linkedin.com/in/vish1al/' className={`${Styles.mailtip} ${Styles[`mailtip_${theme}`]}`}>   
          <img src={linkedinIcon} alt='linkedin icon' className={Styles.images} />    
          <a href='https://www.linkedin.com/in/vish1al/' target="_blank" className={Styles.mailtiptext}>Linkedin</a>
          <div className={Styles.mailtipText}>linkedin</div>
       </a>
-      <a target="_blank" href='https://github.com/VISH01AL' className={Styles.mailtip}>
+      <a target="_blank" href='https://github.com/VISH01AL'  className={`${Styles.mailtip} ${Styles[`mailtip_${theme}`]}`}>
         <img src={githubIcon} alt='github icon' className={Styles.images}  />
         <a href='https://github.com/VISH01AL' target="_blank" className={Styles.mailtiptext}>github</a>
         <div className={Styles.mailtipText}>Github</div>
       </a>
-      <a target="_blank" href='https://www.geeksforgeeks.org/user/vishalg1' className={Styles.mailtip}>
+      <a target="_blank" href='https://www.geeksforgeeks.org/user/vishalg1'  className={`${Styles.mailtip} ${Styles[`mailtip_${theme}`]}`}>
         <img src={geeksforgeeksIcon} alt='github icon' className={Styles.images}  />
         <a href='https://www.geeksforgeeks.org/user/vishalg1' target="_blank" className={Styles.mailtiptext}>geeks for geeks</a>
         <div className={Styles.mailtipText}>GeeksForGeeks</div>
